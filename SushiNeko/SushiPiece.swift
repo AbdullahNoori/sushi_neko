f41351fe2b1a37770f5eecc9fa5b65198779282e
AbdullahNoori <nooriabdullah86@gmail.com>
2020-03-03 01:46:58 -0800

    updates

--- SushiNeko/SushiPiece.swift
+++ SushiNeko/SushiPiece.swift
@@ -1,4 +1,4 @@
-import SpriteKit
+simport SpriteKit
 
 class SushiPiece: SKSpriteNode {
 
---
+++ SushiNeko/reference:helpers/Constants.swift
@@ -0,0 +1,23 @@
+//
+//  Constants.swift
+//  SushiNeko
+//
+//  Created by Macbook Pro 15 on 02/29/19.
+//  Copyright © 2019 Abdullah Noori. All rights reserved.
+//
+
+let kPUNCH: String = "punch"
+let kBACKGROUND: String = "background"
+let kBASESUSHI: String = "sushiBasePiece"
+let kLEFTCHOPSTICK: String = "leftChopstick"
+let kRIGHTCHOPSTICK: String = "rightChopstick"
+let kCHARACTER: String = "character"
+let kPLAYBUTTON: String = "playButton"
+let kHEALTHBARBACKGROUND:String = "healthBarBackground"
+let kHEALTHBAR:String = "healthBar"
+let kSCORELABEL:String = "scoreLabel"
+let kGAMETITLELABEL: String = "gameTitleLabel"
+let kHIGHSCORELABEL: String = "highScoreLabel"
+let kTAPTOPLAYLABEL:String = "tapToPlayLabel"
+let kGAMEMAT: String = "gameMat"
+let kHIGHSCORE: String = "highScore"
