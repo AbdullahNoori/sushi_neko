24ae5e32cf62badaa6682f9984e059f09ba8acbc
AbdullahNoori <nooriabdullah86@gmail.com>
2020-03-08 07:23:03 -0700

    file re arrange

---
+++ sushi_neko/SushiNeko/Character.swift
@@ -0,0 +1,39 @@
+//
+//  GameScene.swift
+//  SushiNeko
+
+
+import SpriteKit
+
+@available(iOS 9.0, *)
+class Character: SKSpriteNode {
+    
+    let Punch = SKAction(named: "Punch")!
+    
+    /* Character side */
+    
+    var side: Side = .left {
+        didSet {
+            if side == .left {
+                xScale = 1
+                position.x = 70
+            } else {
+                /* An easy way to flip an asset horizontally is to invert the X-axis scale */
+                xScale = -1
+                position.x = 252
+            }
+            /* Run the punch action */ run(Punch)
+        }
+    }
+    
+
+    /* You are required to implement this for your subclass to work */
+    override init(texture: SKTexture?, color: UIColor, size: CGSize) {
+        super.init(texture: texture, color: color, size: size)
+    }
+
+    /* You are required to implement this for your subclass to work */
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
